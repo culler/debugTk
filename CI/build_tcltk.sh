2b2c2dfaad643c8eabddb8b5a83dc5f50c5a8a37
Marc Culler <culler@users.noreply.github.com>
2024-05-26 13:00:26 -0500

    Fix path

--- CI/build_tcltk.sh
+++ CI/build_tcltk.sh
@@ -4,8 +4,8 @@ tar xfz tcl8.6.14-src.tar.gz
 mv tcl8.6.14 tcl
 mkdir -p build/tk/Development
 mkdir build/tk/Deployment
-cp CI/*.plist build/tk/Development
-cp CI/*.plist build/tk/Deployment
+cp tk/CI/*.plist build/tk/Development
+cp tk/CI/*.plist build/tk/Deployment
 cd tcl
 make -C macosx
 cd ../tk
