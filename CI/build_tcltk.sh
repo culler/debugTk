f074eba31dd8272a31d06163fb74c524a87fc872
Marc Culler <culler@users.noreply.github.com>
2024-05-26 12:58:47 -0500

    What if we just copy the plist files in place?

--- CI/build_tcltk.sh
+++ CI/build_tcltk.sh
@@ -2,6 +2,10 @@ set -e
 curl -OL https://prdownloads.sourceforge.net/tcl/tcl8.6.14-src.tar.gz
 tar xfz tcl8.6.14-src.tar.gz
 mv tcl8.6.14 tcl
+mkdir -p build/tk/Development
+mkdir build/tk/Deployment
+cp CI/*.plist build/tk/Development
+cp CI/*.plist build/tk/Deployment
 cd tcl
 make -C macosx
 cd ../tk
